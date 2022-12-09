5a581b854850c4cc0df51dcefab494939f91eccc
Shawn Johnson <shawn.johnson@sysdig.com>
2022-12-09 15:00:40 -0500

    Change sysdig-cloud to use app runner deployment

--- sysdig-cloud-aws.tf
+++ sysdig-cloud-aws.tf
@@ -3,8 +3,8 @@ provider "sysdig" {
   sysdig_secure_api_token = var.sysdig_secure_api_token
 }
 
-module "secure_for_cloud_aws_single_account_k8s" {
-  source = "sysdiglabs/secure-for-cloud/aws//examples/single-account-k8s"
-  benchmark_regions	= ["us-east-1"]
+module "secure_for_cloud_aws" {
+  source                           = "sysdiglabs/secure-for-cloud/aws//examples/single-account-apprunner"
+  benchmark_regions                = ["us-east-1"]
   cloudtrail_is_multi_region_trail = false
 }
